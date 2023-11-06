a12cfcdc8686d2bfbfd9b28fad8c2e3110a27979
koralle <mkhd7930@gmail.com>
2023-11-06 21:49:24 +0900

    chore(colorscheme): set `Everforest Dark (Gogh)`

--- wezterm.lua
+++ wezterm.lua
@@ -14,7 +14,7 @@ config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 
 -- Colorscheme
-config.color_scheme = "tokyonight_storm"
+config.color_scheme = "Everforest Dark (Gogh)"
 
 -- Font
 config.font = wezterm.font("Cica")
